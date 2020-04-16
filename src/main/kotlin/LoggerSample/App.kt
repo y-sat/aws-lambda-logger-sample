e3b4b46afd18e58026ef437ce79c034ac2eec694
y-satake <yuki.satake@c-pistas.com>
2020-04-16 09:57:00 +0900

    first commit

---
+++ src/main/kotlin/LoggerSample/App.kt
@@ -0,0 +1,16 @@
+package LoggerSample
+
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+class App : RequestHandler<Any?, Any> {
+    private val logger: Logger = LogManager.getLogger(App::class.java)
+    override fun handleRequest(input: Any?, context: Context) {
+        logger.info("log data from log4j info.")
+        logger.debug("log data from log4j debug.")
+        logger.warn("log data from log4j warn.")
+        logger.error("log data from log4j err.")
+    }
+}
